268964728d45a59b4a13b85ee651f84eba148fac
Ben Kircher <bkircher@0xadd.de>
2023-04-15 14:35:37 +0200

    Use a better prompt

--- chatcli/chat.py
+++ chatcli/chat.py
@@ -2,16 +2,17 @@ import os
 from typing import Callable
 
 import openai
+from prompt_toolkit import PromptSession
 
 
-def repl(evaluate: Callable[[str], str]) -> None:
+def repl(session: PromptSession, eval_fn: Callable[[str], str]) -> None:
     while True:
         try:
-            text = input(">>> ")
+            text = session.prompt(">>> ")
         except (EOFError, KeyboardInterrupt):
             return
         if text:
-            output = evaluate(text)
+            output = eval_fn(text)
             print(output)
         else:
             # Wait for more input
@@ -29,9 +30,10 @@ def chat(text: str) -> str:
 def main() -> None:
     openai.api_key = os.getenv("OPENAI_API_KEY")
     if not openai.api_key:
-        raise RuntimeError("OPENAI_API_KEY env var is not set")
+        raise RuntimeError("OPENAI_API_KEY env var is not set or empty")
 
-    repl(chat)
+    session = PromptSession()
+    repl(session=session, eval_fn=chat)
 
 
 if __name__ == "__main__":
